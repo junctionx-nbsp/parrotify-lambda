b4210cdad7b170d0e55f840fa8672e457e53ea4c
Lukas Tetzlaff <lukastetzlaff@gmail.com>
2018-10-20 17:12:40 +0200

    :wrench: update serverless config and index file

--- src/index.ts
+++ src/index.ts
@@ -1,5 +1,7 @@
 import "./utils/envs"
 
 import hello from "./handlers/hello"
+import textToMp3 from "./handlers/text-to-mp3"
+import mp3ToWav from "./handlers/mp3-to-wav"
 
-export { hello }
+export { hello, textToMp3, mp3ToWav }
