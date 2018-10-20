f03581a7ab77e7673bde5fdc543afd402d199916
Lukas Tetzlaff <lukastetzlaff@gmail.com>
2018-10-21 01:40:27 +0200

    :hammer: :bug: fix faulty mp3-to-wav conversion

--- src/handlers/mp3-to-wav.ts
+++ src/handlers/mp3-to-wav.ts
@@ -1,43 +1,45 @@
 import { APIGatewayEvent, Callback, Context, Handler } from "aws-lambda"
 import ffmpeg from "../utils/ffmpeg"
 import { IStringIndexed } from "improved/dist/types"
-import { unlink, getFileName } from "improved/dist/fs"
+import { unlink } from "improved/dist/fs"
 import random from "../utils/random"
-import { resolve } from "path"
 import { S3_BUCKET } from "../utils/envs"
 import { S3 } from "aws-sdk"
-import { createReadStream } from "fs"
+import { createReadStream, statSync } from "fs"
 
 const mp3ToWav: Handler = async (event: APIGatewayEvent, _: Context, cb: Callback) => {
   const { inputUrl }: IStringIndexed = event.queryStringParameters || {}
 
-  const temporaryFile = resolve("~/temp", random() + ".wav")
+  const temporaryFile = `/tmp/${random()}.wav`
+
+  console.log(`Temporary File: ${temporaryFile}`)
   await ffmpeg(["-i", inputUrl, "-acodec", "pcm_alaw", "-ar", "8000", "-ac", "1", temporaryFile])
 
-  const temporaryFileStream = createReadStream(temporaryFile)
+  console.log(`Done with ffmpeg, file stats: ${JSON.stringify(statSync(temporaryFile))}`)
   const s3 = new S3()
-
   const fileName = inputUrl.split("/").pop()!.replace(/\.mp3/i, ".wav")
-  s3.putObject({
-    Body: temporaryFileStream,
+
+  console.log(`Starting S3 upload to ${S3_BUCKET}/${fileName}`)
+  const upload = s3.upload({
+    Body: createReadStream(temporaryFile),
     Bucket: S3_BUCKET!,
     Key: fileName,
-    ContentType: "audio/wav",
     ACL: "public-read"
-  }, (err, data) => {
-    if (err) {
-      console.log("Error writing to S3", err)
-      return cb(err)
-    }
+  })
+  try {
+    const data = await upload.promise()
     cb(undefined, {
       statusCode: 200,
-      body: JSON.stringify({ url: `https://${S3_BUCKET}.s3.amazonaws.com/${encodeURIComponent(fileName)}` })
+      body: JSON.stringify({
+        url: `https://${S3_BUCKET}.s3.amazonaws.com/${encodeURIComponent(fileName)}`
+      })
     })
-
-    temporaryFileStream.on("close", async () => {
-      await unlink(temporaryFile)
-    })
-  })
+  } catch (e) {
+    console.log("Error writing to S3", e)
+    cb(e)
+  } finally {
+    await unlink(temporaryFile)
+  }
 }
 
 export default mp3ToWav
